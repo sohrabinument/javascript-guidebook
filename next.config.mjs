0748544b9af3bc2555962f227015db59be3a695e
sajalkhan <sajalhossen@yahoo.com>
2025-05-04 10:00:53 +0600

    update config and add interview question

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,18 @@ import {
 
 import nextra from "nextra";
 /** @type {import('next').NextConfig} */
-const config = {};
+
+const redirects = async () => [
+  {
+    source: "/",
+    destination: "/javascript-guidebook-bn",
+    permanent: false,
+  },
+];
+
+const config = {
+  redirects,
+};
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
@@ -13,30 +24,27 @@ const withNextra = nextra({
   defaultShowCopyCode: true,
 });
 
-
-
-
 const nextConfigFunction = async (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    const withPWA = (await import('@ducanh2912/next-pwa')).default({
-      dest: 'public',
+    const withPWA = (await import("@ducanh2912/next-pwa")).default({
+      dest: "public",
       cacheOnFrontEndNav: true,
       aggressiveFrontEndNavCaching: true,
       reloadOnOnline: true,
       swcMinify: true,
-      disable: process.env.NODE_ENV === 'development',
+      disable: process.env.NODE_ENV === "development",
       // disable: false,
       workboxOptions: {
         disableDevLogs: true,
       },
       fallbacks: {
         // Failed page requests fallback to this.
-        document: '/~offline',
+        document: "/~offline",
       },
-    })
-    return withPWA(withNextra(config))
+    });
+    return withPWA(withNextra(config));
   }
-  return nextConfig
-}
-export default nextConfigFunction
+  return nextConfig;
+};
 
+export default nextConfigFunction;
